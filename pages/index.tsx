cc96ea5dd801b09b6cf834f1b22674224a6e1979
rukysandy <rukysandy57@gmail.com>
2022-09-30 20:35:31 +0100

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Box } from "@chakra-ui/layout";
+import Head from "next/head";
+import Image from "next/image";
+
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return <Box>Home</Box>;
+}
